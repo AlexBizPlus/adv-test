26bea2c5dc800846fede384f69353de913300807
Нежнов Алексей Вячеславович <anezhnov@nntc.pro>
2024-06-10 15:28:44 +0700

    [WIP]

---
+++ script.js
@@ -0,0 +1,15 @@
+const triggers = document.querySelectorAll(".tooltip__trigger");
+
+const toggleMenu = (e) => {
+  const template = document.querySelector("#template-tooltip");
+  const tooltip = template.content.cloneNode(true);
+  const wrapper = tooltip.querySelector(".tooltip__text");
+  const button = tooltip.querySelector(".tooltip__button");
+
+  const removeTooltip = () => wrapper?.remove();
+
+  button.addEventListener("click", removeTooltip);
+  e.target.after(tooltip);
+};
+
+triggers.forEach((trigger) => trigger.addEventListener("click", toggleMenu));
